65db8e28a6e8ef295770da174a97d3a3db42ed29
ImranDevify <imranguru360@gmail.com>
2024-06-28 22:19:30 +0500

    project uploadde

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full bg-black'>
+      <h1 className='text-red-600'>Hello, World</h1>
+    </div>
+  )
+}
+
+export default App
